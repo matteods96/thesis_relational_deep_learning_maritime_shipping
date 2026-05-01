92b489d2a6fb2594042d17f5e6b6e52d2a40af27
matteods96 <s242947@dtu.dk>
2026-05-01 16:57:56 +0200

    Add python script and job script for identifying distinct AIS ports as preprocessing step

---
+++ jobs/submit_find_ais_distinct_ports.sh
@@ -0,0 +1,37 @@
+#!/bin/sh 
+### General options 
+#BSUB -q hpc
+#BSUB -J jobAISPorts
+#BSUB -n 4
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=27GB]"
+#BSUB -R "select[model == XeonGold6226R]"
+#BSUB -M 27GB
+#BSUB -W 24:00 
+#BSUB -u s242947@dtu.dk
+#BSUB -B 
+#BSUB -N 
+#BSUB -o jobAISFindDistinctPorts_%J.out 
+#BSUB -e jobAISFindDistinctPorts_%J.err 
+
+# Load Python and CUDA
+module load python3/3.11.13 
+module load cuda/11.7
+
+# Create virtual environment (only if missing)
+if [ ! -d /dtu/blackhole/08/213928/venv ]; then
+    python3 -m venv /dtu/blackhole/08/213928/venv
+fi
+
+# Activate your virtual environment 
+source /dtu/blackhole/08/213928/venv/bin/activate 
+
+# Install required Python packages INSIDE the venv
+pip install --upgrade pip
+pip install duckdb requests geopandas pyarrow openpyxl
+
+# Move to your project folder 
+cd /dtu/blackhole/08/213928/storage
+
+# Run your script
+python ais_find_distinct_ports.py
