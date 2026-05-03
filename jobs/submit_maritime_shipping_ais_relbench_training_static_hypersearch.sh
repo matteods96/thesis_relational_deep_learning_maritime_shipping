aa8aac2fc74d4b0fd5d4b62d5e97cb066293b634
matteods96 <s242947@dtu.dk>
2026-05-03 14:24:19 +0200

    Add hyperparameter search for heterogenuos graph sage model python script and job script for static training task

---
+++ jobs/submit_maritime_shipping_ais_relbench_training_static_hypersearch.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#BSUB -q gpua100
+#BSUB -J jobMaritimeShippingAISdbRelbenchTrainingStaticHyperSearch
+#BSUB -n 4
+##BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -gpu "num=1"
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=128GB]"
+#BSUB -M 128GB
+#BSUB -W 24:00
+#BSUB -u s242947@dtu.dk
+#BSUB -B
+#BSUB -N
+#BSUB -o jobMaritimeShippingAISDKRelbenchTrainingStaticHyperSearch_%J.out
+#BSUB -e jobMaritimeShippingAISDKRelbenchTrainingStaticHyperSearch_%J.err
+
+nvidia-smi
+
+module load python3/3.11.13
+module load cuda/11.7
+
+source /dtu/blackhole/08/213928/venv/bin/activate
+
+cd /dtu/blackhole/08/213928/storage
+
+python maritime_shipping_ais_relbench_training_static_hypersearch.py
