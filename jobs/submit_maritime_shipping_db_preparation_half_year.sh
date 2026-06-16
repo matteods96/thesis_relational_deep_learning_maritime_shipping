7f86ec56dea34e8e6ec1088fc3bc805426413aaa
matteods96 <s242947@dtu.dk>
2026-06-16 20:57:05 +0200

    uploadin maritime shipping db for first 180 day in 2025

---
+++ jobs/submit_maritime_shipping_db_preparation_half_year.sh
@@ -0,0 +1,40 @@
+#!/bin/sh 
+### General options 
+#BSUB -q hpc
+#BSUB -J jobMaritimeShippingDbPreparation
+#BSUB -n 4
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=27GB]"
+#BSUB -R "select[model == XeonGold6226R]"
+#BSUB -M 27GB
+#BSUB -W 24:00 
+#BSUB -u s242947@dtu.dk
+#BSUB -B 
+#BSUB -N 
+
+#BSUB -o jobMaritimeShippingDbPreparation_%J.out 
+#BSUB -e jobMaritimeShippingDbPreparation_%J.err 
+
+
+
+# Load Python and CUDA
+module load python3/3.11.13 
+module load cuda/11.7
+
+# Create virtual environment (only if missing)
+if [ ! -d /dtu/blackhole/08/213928/venv ]; then
+    python3 -m venv /dtu/blackhole/08/213928/venv
+fi
+
+# Activate your virtual environment 
+source /dtu/blackhole/08/213928/venv/bin/activate 
+
+# Install required Python packages INSIDE the venv
+pip install --upgrade pip
+pip install duckdb requests geopandas pyarrow openpyxl relbench
+
+# Move to your project folder 
+cd /dtu/blackhole/08/213928/storage
+
+# Run your script
+python maritime_shipping_db_preparation_half_year.py
